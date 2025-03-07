eb04c90829f00985f2a14b825d0cfa564aa653fe
llamees4 <lamees.abuajamiej11@gmail.com>
2025-03-07 22:12:26 +0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let num=[1,22,56,33,75,99,48]
-
+// use map and filter methods
 fun1=num.map((x) => x/2);
 fun2=num.filter(x=>x%2==0);
-
+// show results
 console.log("numbers before functions ",num)
 console.log('Half the number ? ' ,fun1)
 console.log(`only even numbers [ ${fun2}  ]`)
-console.log("done")
